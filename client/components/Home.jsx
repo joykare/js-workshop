f99f68aeb5c54b7f79fabb2876f3bc9e40f6656a
andela-jkimani <jackline.kimani@andela.com>
2017-04-20 01:50:23 +0300

    Home Page

---
+++ client/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => (
+  <div style={{ textAlign: 'center' }}>
+    <h1>BookShop App</h1>
+    <p>Manage your book list with our simple app.</p>
+  </div>);
+
+module.exports = Home;
