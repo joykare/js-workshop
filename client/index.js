19790315008f304dadef960342b490bd6680c7d4
Jackline Kimani <andela-jkimani@users.noreply.github.com>
2017-04-20 20:28:44 +0300

    Revert "Create movies"

--- client/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AddMovies from './containers/AddMovies.jsx';
-
-
-ReactDOM.render(<AddMovies />, document.getElementById('root'));
