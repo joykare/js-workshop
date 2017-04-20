c22c6e31d37c6261223528a1122b1354ba0d7d4b
joykare <joy.warugu@andela.com>
2017-04-20 17:43:05 +0300

    General refactor

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -3,16 +3,18 @@ import React from 'react';
 
 const Home = () => (
   <div>
-    <h1>BookShop App</h1>
-    <p>Manage your book list with our simple app.</p>
+    
   </div>);
 
 class BookItems extends React.Component {
   render() {
+    console.log("props", this.props.entries);
     const bookEntries = this.props.entries;
 
-    const createBooks = book => <li key={book.key}>{book.text}</li>;
-    const listBooks = bookEntries.map(createBooks);
+    // const createBooks = (book) => <li key={book.key}>{book.title}</li>;
+    const listBooks = bookEntries.map(book => (
+      <li key={book.key}>{book.title}</li>
+    ));
     return (
       <ul>
         {listBooks}
@@ -22,43 +24,57 @@ class BookItems extends React.Component {
 }
 
 export default class BookList extends React.Component {
-  getInitialState() {
-    return {
+  constructor(props) {
+    super(props);
+    this.state = {
       books: [],
+      book: {}
     };
+    this.addBook = this.addBook.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
   // add event handler
   addBook(e) {
-    const bookArray = this.state.books;
-    console.log(this.state.books);
-    bookArray.push(
-      {
-        text: this._inputElement.value,
-        key: Date.now(),
-      },
-  );
-
-    this.setState({
-      books: bookArray,
-    });
-    this._inputElement.value = '';
     e.preventDefault();
+
+    const bookArray = this.state.books;
+    bookArray.push(
+      this.state.book
+    );
+    this.setState({
+      book: {},
+      books: bookArray
+    })
+    console.log("books", this.state.books);
+    
+  }
+
+  handleChange(e) {
+    e.preventDefault();
+    this.setState({
+      book: { 
+        title: e.target.value,
+        key: Date.now()
+      }
+    });
+    console.log(this.state.bookTitle);
   }
 
   render() {
     return (
       <div className="todoListMain">
-        <Home />
+        <h1>BookShop App</h1>
+        <p>Manage your book list with our simple app.</p>
         <div className="header">
           <form onSubmit={this.addBook}>
             <input
-              ref={a => this._inputElement = a}
+              onChange={this.handleChange}
               placeholder="enter book name"
             />
             <button type="submit">add</button>
           </form>
+          <BookItems entries={this.state.books} />
         </div>
-        <BookItems entries={this.state.books} />
       </div>
     );
   }
