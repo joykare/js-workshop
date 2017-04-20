a629b3c69916f048ff4c558bca81f9e994f64f05
joykare <joy.warugu@andela.com>
2017-04-20 21:53:10 +0300

    Refactor code

--- client/components/EditMovies.jsx
+++ client/components/EditMovies.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 
 const EditMovies = (props) => {
-  const {handleDelete, handleUpdate} = props;
+  const { handleDelete, handleUpdate, entries } = props;
   return (
     <div>
-      <div className='card' style={{margin:500, width: 300}}>
-        <div className="card-block">
-          <h4 className="card-title">Orange is the new Black</h4>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <button className="btn btn-primary" onClick={handleUpdate}>Update</button>
-          <button className="btn btn-danger" onClick={handleDelete} style={{marginLeft: 10}}>Delete</button>
-        </div>
-      </div>
+      <div className='row col-lg-6 offset-lg-4'>
+        {entries.map(book => (
+          <div className='card' key={book.title} style={{ width: 300, margin:20 }}>
+            <div className="card-block">
+              <h4 className="card-title">{book.title}</h4>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <button className="btn btn-primary" onClick={() => handleUpdate(book.title)}>Update</button>
+              <button className="btn btn-danger" onClick={() => handleDelete(book.title)} style={{marginLeft: 10}}>Delete</button>
+            </div>
+          </div>
+        ))}
+      </div>  
     </div>
   );
 };
--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import EditMoviesContainer from './containers/EditMoviesContainer.jsx';
+import MovieDisplayContainer from './containers/MovieDisplayContainer.jsx';
 
-ReactDOM.render(<EditMoviesContainer />, document.getElementById('root'));
+ReactDOM.render(<MovieDisplayContainer />, document.getElementById('root'));
